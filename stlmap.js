aadd2f6e6f6d674e1d8e4ec1acef45fc89606f63
Carrie Chunn <crchunn@gmail.com>
2020-11-04 10:56:54 -0600

    setting up the basics

--- stlmap.js
+++
@@ -1,9 +0,0 @@
-// function sites
-
-
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-  
